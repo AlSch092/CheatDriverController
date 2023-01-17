578b7aa4eb4a8adee6f5d4e17e1ce1f67e20be91
alex <94417808+AlSch092@users.noreply.github.com>
2023-01-17 14:57:08 -0500

    Update CheatDriverController.cpp

--- CheatDriverController.cpp
+++ CheatDriverController.cpp
@@ -15,27 +15,22 @@ int main(int argc, char** argv)
 		//convert ascii bytes to LPBYTE
 	}
 
-	//const BYTE GameObjectManagerPattern[] = { 0x89,0x5C,0x24,'?','?',0x48,0x89,0x74,0x24,'?','?',0x57,0x48,0x83,0xEC,'?','?',0x48,0x8D,0x59,'?','?',0x48,0x8B,0xF9,0x48,0x8B,0x0B };
-
-  BYTE ret[] = { 0xC3 }; //bytes which will be written to the remote process and then executed as a system thread. be careful for BSODs!
+        BYTE ret[] = { 0xC3 }; //bytes which will be written to the remote process and then executed as a system thread. be careful for BSODs!
 
 	if (DriverController::WriteAndExecuteShell(DriverController::FindProcessId(L"notepad.exe"), 0x7ff787d23000, (const LPBYTE)ret, 1, NULL))
 	{
 		printf("Wrote shell and executed!\n");
 	}
 
-	BYTE buff[28] = { 0 };
+	BYTE buff[28] = { 0 }; //read memory buffer
+	
 	if (DriverController::ReadMemory(DriverController::FindProcessId(L"notepad.exe"), 0x7ff787d23000, buff, 1, "./result.out"))
 	{
 		printf("Read memory!\n");
 	}
 	
-  //we can also turn out driver into a pattern scanner
-  //unsigned long long addr = Pattern::FindPattern(L"Albion-Online.exe", GameObjectManagerPattern, "xxx??xxxx??xxxx??xxx??xxxxxx", 0, DriverController::GetBaseAddress(L"Albion-Online.exe"), FALSE); 
-	
-	//if(addr != 0)
-	//	printf("Addr: %llX\n", addr);
+         //we can also turn out driver into a pattern scanner! The process below is normally protected by an anti-cheat driver, which we can get past now
+         //unsigned long long addr = Pattern::FindPattern(L"Albion-Online.exe", GameObjectManagerPattern, "xxx??xxxx??xxxx??xxx??xxxxxx", 0, DriverController::GetBaseAddress(L"Albion-Online.exe"), FALSE); 
 
-	system("pause");
 	return 0;
 }
